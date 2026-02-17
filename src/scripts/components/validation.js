5631a69c385d1d54ca510ff31038ba12f603d9f1
Anarchy73 <kanalcraba@gmail.com>
2026-02-17 20:03:55 +0400

    add checkInputValidity

--- src/scripts/components/validation.js
+++ src/scripts/components/validation.js
@@ -9,8 +9,25 @@ export const showInputError = (settings, formInput, formElement, errorMessage) =
   }
 };
 
-const hideInputError = (settings) => {
+const hideInputError = (settings, formInput, formElement) => {
+  const formError = formElement.querySelector(`#${formInput.id}-error`);
+  formInput.classList.remove(settings.inputErrorClass);
+  formError.classList.remove(settings.errorClass);
+  formError.textContent = "";
+};
 
+const checkInputValidity = (settings, formInput, formElement) => {
+  if (formInput.validity.patternMismatch) {
+    formInput.setCustomValidity(formInput.data-error-message);
+  } else {
+    formInput.setCustomValidity("");
+  }
+
+  if (!formInput.validity.valid) {
+    showInputError(settings, formInput, formElement, formInput.validationMessage);
+  } else {
+    hideInputError(settings, formInput, formElement);
+  }
 };
 
 const hasInvalidInput = (inputList) => {
