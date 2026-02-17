a2eefeef0c7a7bc596ff99d946eaf5172ec69d9c
Anarchy73 <kanalcraba@gmail.com>
2026-02-17 19:41:06 +0400

    showInputError added

--- src/scripts/components/validation.js
+++ src/scripts/components/validation.js
@@ -1,9 +1,16 @@
-const showInputError = (settings, errorMessage) => {
-
+export const showInputError = (settings, formInput, formElement, errorMessage) => {
+  const formError = formElement.querySelector(`#${formInput.id}-error`);
+  formInput.classList.add(settings.inputErrorClass);
+  formError.classList.add(settings.errorClass);
+  if (errorMessage && formInput.validity.patternMismatch){
+    formError.textContent = errorMessage;
+  } else {
+    formError.textContent = formInput.validationMessage;
+  }
 };
 
 const hideInputError = (settings) => {
-  
+
 };
 
 const hasInvalidInput = (inputList) => {
